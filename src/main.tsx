cfbb8e40186d9bba796561c072c9e4d6e2531657
RobertTate <robertjosephtate@gmail.com>
2024-02-04 15:57:26 -0600

    remove the scrollbar

--- src/main.tsx
+++ src/main.tsx
@@ -6,27 +6,6 @@ import App from "./App.tsx";
 import "./dbInstance.ts";
 import "./index.css";
 
-type ExtendedHTMLElement = HTMLElement & {
-  scrollTimeout?: number;
-};
-
-// Select the scrollable container
-const scrollContainer: ExtendedHTMLElement | null =
-  document.querySelector(".scroll-container");
-
-if (scrollContainer) {
-  // Add event listener for scroll event
-  window.addEventListener("scroll", () => {
-    scrollContainer.classList.add("scrolling");
-
-    // Clear the class after a delay
-    clearTimeout(scrollContainer.scrollTimeout);
-    scrollContainer.scrollTimeout = setTimeout(() => {
-      scrollContainer.classList.remove("scrolling");
-    }, 300); // Timeout to remove class
-  });
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
