a3d9028fb39d800daaabc0c59b7e260a99a55712
Robert Tate <37939808+RobertTate@users.noreply.github.com>
2024-08-10 10:20:39 -0500

    add analytics and speed insights components (#10)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import "@mdxeditor/editor/style.css";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -9,5 +11,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 );
